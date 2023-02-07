9a97faac37bc1026c5518fb602521a993508158e
Stoneblackdog <rabelkrisy@gmail.com>
2023-02-07 17:24:00 +0100

    lua stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("stoneblackdog")
+require('plugins')
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
--- .config/nvim/lua/stoneblackdog/init.lua
+++ .config/nvim/lua/stoneblackdog/init.lua
@@ -1,5 +1,6 @@
 require("stoneblackdog.remap")
 require("stoneblackdog.set")
 require('lualine').setup()
+require('plugins')
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
