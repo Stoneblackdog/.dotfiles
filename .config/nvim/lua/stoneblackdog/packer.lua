2ff53659772ab6777d2604fa67d34a41e18a927d
Kristóf Rábel <rabelkrisy@gmail.com>
2023-03-21 18:52:10 +0100

    sticky headers

--- .config/nvim/lua/stoneblackdog/packer.lua
+++ .config/nvim/lua/stoneblackdog/packer.lua
@@ -19,6 +19,7 @@ return require('packer').startup(function(use)
   use({ "ellisonleao/gruvbox.nvim" })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter-context')
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
 
